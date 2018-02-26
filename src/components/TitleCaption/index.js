b421845372d6a060d4d23fa6ae9d9aded729661e
Yeung2017 <ciutzhaoyun@163.com>
2018-02-26 15:09:45 +0800

    add TitleCaption

---
+++ src/components/TitleCaption/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+const TitleCaption = ({ title, subTitle, desc }) => {
+  const cTitle = title ? <h1 className="c-TitleCaption__title">{ title }</h1> : null;
+  const cSubTitle = subTitle ? <h2 className="c-TitleCaption__subTitle">{ subTitle }</h2> : null;
+  const cDesc =  desc ? <p className="c-TitleCaption__desc">{desc}</p> : null;
+  return (
+    <div className="c-TitleCaption">
+      {cTitle}
+      {cSubTitle}
+      {cDesc}
+    </div>
+  );
+};
+
+TitleCaption.propTypes = {
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  desc: PropTypes.string,
+};
+
+TitleCaption.defaultProps = {
+  title: '',
+  subTitle: '',
+  desc: ''
+};
+
+export default TitleCaption;
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TopNavBar from '../../components/TopNavBar';
+import TitleCaption from '../../components/TitleCaption';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
 
 const PageIndex = () => {
@@ -9,6 +11,7 @@ const PageIndex = () => {
       <header className="page-index__header">
         <div className="container">
           <TopNavBar />
+          <TitleCaption title="BootCDN" subTitle="稳定、快速、免费的前端开源项目 CDN 加速服务" desc="共收录了 3318 个前端开源项目" />
         </div>
       </header>
     </div>
