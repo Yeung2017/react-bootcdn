56faea2668aee449ed7666aaaa4c0e3ce5f42b1b
Yeung2017 <ciutzhaoyun@163.com>
2018-02-26 16:28:40 +0800

    add Search

---
+++ src/components/Search/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './index.css';
+
+export class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+      isFocus: false
+    };
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+    this.props.onChange();
+  }
+
+  handleSearchFocus = () => {
+    this.setState({
+      isFocus: true
+    });
+  }
+
+  handleSearchBlur = () => {
+    this.setState({
+      isFocus: false
+    });
+  }
+
+  handleClearIconClick = (e) => {
+    this.setState({
+      filter: ''
+    });
+  }
+
+  handleSearchIconClick = () => {
+    this.props.onSearchIconClick(this.state.filter);
+  }
+
+  render() {
+    return (
+      <div className={ this.state.isFocus ? 'c-search is-active' : 'c-search' }>
+        <input className="c-search__input" type="text" name="filter" placeholder="搜索开源库，例如：jquery" vlaue={ this.state.text } onChange={ this.handleInputChange } onFocus={ this.handleSearchFocus } onBlur={ this.handleSearchBlur } />
+        <i className={ this.state.filter !== '' ? "iconfont icon-cuowuguanbiquxiao c-search__icon" : "iconfont icon-sousuo c-search__icon" } onClick={this.state.filter === '' ? this.handleSearchIconClick : this.handleClearIconClick }></i>
+      </div>
+    );
+  }
+}
+
+Search.propTypes = {
+  onChange: PropTypes.func,
+  onSearchIconClick: PropTypes.func,
+};
+
+Search.defaultProps = {
+  onChange: f => f,
+  onSearchIconClick: f => f
+};
+
+export default Search;
+
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TopNavBar from '../../components/TopNavBar';
 import TitleCaption from '../../components/TitleCaption';
+import Search from '../../components/Search';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
@@ -12,6 +13,11 @@ const PageIndex = () => {
         <div className="container">
           <TopNavBar />
           <TitleCaption title="BootCDN" subTitle="稳定、快速、免费的前端开源项目 CDN 加速服务" desc="共收录了 3318 个前端开源项目" />
+          <div className="row justify-content-md-center">
+            <div className="col-md-8">
+              <Search />
+            </div>
+          </div>
         </div>
       </header>
     </div>
