1d7d0b289c4b82ff25435ecee27c23a0d32e35d6
Yeung2017 <ciutzhaoyun@163.com>
2018-03-13 09:03:10 +0800

    update

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
 import libs from './state/libs/reducer';
 import libsFilter from './state/libsFilter/reducer';
-import libDetails from './state/libDetails/reducer';
+import libDetails from './state/LibDetails/reducer';
 
 export default combineReducers({libs, libsFilter, libDetails});
