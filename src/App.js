ecfd75458d443201debfd65e8e97f4a34b3285d0
Yeung2017 <ciutzhaoyun@163.com>
2018-04-08 20:16:40 +0800

    revoke split code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './redux/store';
-import PageIndex from './pages/Index';
-import PageDetails from './pages/Details';
-import PageBlog from './pages/Blog';
-import PageApi from './pages/Api';
-import PageAbout from './pages/About';
+import Loadable from 'react-loadable';
+import Loading from './components/Loading';
 
 import './App.css';
+
+const PageBlog = Loadable({
+  loader: ()=>import('./pages/Blog'),
+  loading: Loading
+});
+
+const PageIndex = Loadable({
+  loader: ()=>import('./pages/Index'),
+  loading: Loading
+});
+
+const PageDetails = Loadable({
+  loader: ()=>import('./pages/Details'),
+  loading: Loading
+});
+
+const PageApi = Loadable({
+  loader: ()=>import('./pages/Api'),
+  loading: Loading
+});
+
+const PageAbout = Loadable({
+  loader: ()=>import('./pages/About'),
+  loading: Loading
+});
+
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/api" component={PageApi} />
-          <Route path="/blog" component={PageBlog} />
-          <Route path="/about" component={PageAbout} />
-          <Route path="/lib-details/:id" component={PageDetails} />
-          <Route exact path="/:all" component={PageIndex} />
-          <Route exact path="/" component={PageIndex} />
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route path="/api" component={PageApi}/>
+            <Route path="/blog" component={PageBlog}/>
+            <Route path="/about" component={PageAbout}/>
+            <Route path="/lib-details/:id" component={PageDetails}/>
+            <Route exact path="/:all" component={PageIndex}/>
+            <Route exact path="/" component={PageIndex}/>
+          </Switch>
+        </Router>
       </Provider>
     );
   }
